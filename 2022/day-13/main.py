f74560bb59355d9e1f4a5486a2855be09db8a77b
Joshua Kite <joshuamkite@gmail.com>
2024-09-11 19:14:45 +0100

    tidy up for readability

--- 2022/day-13/main.py
+++ 2022/day-13/main.py
@@ -10,30 +10,10 @@ correct_packets = []
 # Create a list of packets to sort them for part 2, seeded with proscripted dividers values
 ordered_packets = [[[2]], [[6]]]
 
-# Open the file and read it line by line
-with open('./input.txt', 'r') as file:
-    # Initialize the group counter
-    group_counter = 1
-
-    # Process file in chunks of three lines
-    while True:
-        # Use islice to get the next three lines
-        lines = list(islice(file, 3))
-
-        if not lines:  # Break if no lines left
-            break
-
-        # Store the first line and the second line in a list as the value for the key by group in dict packets
-        packets[group_counter] = [ast.literal_eval(lines[0].strip()), ast.literal_eval(lines[1].strip())]
-
-        # Increment the group counter
-        group_counter += 1
-
-"""When comparing two values, the first value is called left and the second value is called right. Then:
-
-- If both values are integers, the lower integer should come first. If the left integer is lower than the right integer, the inputs are in the right order. If the left integer is higher than the right integer, the inputs are not in the right order. Otherwise, the inputs are the same integer; continue checking the next part of the input.
-- If both values are lists, compare the first value of each list, then the second value, and so on. If the left list runs out of items first, the inputs are in the right order. If the right list runs out of items first, the inputs are not in the right order. If the lists are the same length and no comparison makes a decision about the order, continue checking the next part of the input.
-- If exactly one value is an integer, convert the integer to a list which contains that integer as its only value, then retry the comparison. For example, if comparing [0,0,0] and 2, convert the right value to [2] (a list containing 2); the result is then found by instead comparing [0,0,0] and [2]."""
+# When comparing two values, the first value is called left and the second value is called right. Then:
+# - If both values are integers, the lower integer should come first. If the left integer is lower than the right integer, the inputs are in the right order. If the left integer is higher than the right integer, the inputs are not in the right order. Otherwise, the inputs are the same integer; continue checking the next part of the input.
+# - If both values are lists, compare the first value of each list, then the second value, and so on. If the left list runs out of items first, the inputs are in the right order. If the right list runs out of items first, the inputs are not in the right order. If the lists are the same length and no comparison makes a decision about the order, continue checking the next part of the input.
+# - If exactly one value is an integer, convert the integer to a list which contains that integer as its only value, then retry the comparison. For example, if comparing [0,0,0] and 2, convert the right value to [2] (a list containing 2); the result is then found by instead comparing [0,0,0] and [2].
 
 
 def compare(left, right):
@@ -107,26 +87,47 @@ class ListSorter:
         return indices
 
 
-# Process each packet and compare
-for packet_num in packets:
-    left_packet, right_packet = packets[packet_num]
-    result = compare(left_packet, right_packet)
-    # print(f"Packet {packet_num}: {'True' if result else 'False'}")
-    if result:
-        correct_packets.append(packet_num)
+def main():
+    # Open the file and read it line by line
+    with open('./input.txt', 'r') as file:
+        # Initialize the group counter
+        group_counter = 1
 
-    # Sort the packets for part 2
-    ordered_packets.append(left_packet)
-    ordered_packets.append(right_packet)
-print(f"Part 1 solution: {sum(correct_packets)}")
+        # Process file in chunks of three lines
+        while True:
+            # Use islice to get the next three lines
+            lines = list(islice(file, 3))
+
+            if not lines:  # Break if no lines left
+                break
+
+            # Store the first line and the second line in a list as the value for the key by group in dict packets
+            packets[group_counter] = [ast.literal_eval(lines[0].strip()), ast.literal_eval(lines[1].strip())]
+
+            # Increment the group counter
+            group_counter += 1
+
+    # Process each packet and compare
+    for packet_num in packets:
+        left_packet, right_packet = packets[packet_num]
+        result = compare(left_packet, right_packet)
+        # print(f"Packet {packet_num}: {'True' if result else 'False'}")
+        if result:
+            correct_packets.append(packet_num)
+
+        # Sort the packets for part 2
+        ordered_packets.append(left_packet)
+        ordered_packets.append(right_packet)
+    print(f"Part 1 solution: {sum(correct_packets)}")
+
+    # Instantiate the ListSorter class
+    sorter = ListSorter(ordered_packets)
+
+    # Find the index positions of [[[2]]] and [[[6]]]
+    indices = sorter.find_indices_of_static_items()
+
+    print("Part 2 solution:", (indices[0] * indices[1]))
 
 
-# Instantiate the ListSorter class
-sorter = ListSorter(ordered_packets)
-
-
-# Find the index positions of [[[2]]] and [[[6]]]
-indices = sorter.find_indices_of_static_items()
-
-
-print("Part 2 solution:", (indices[0] * indices[1]))
+if __name__ == "__main__":
+    main()
